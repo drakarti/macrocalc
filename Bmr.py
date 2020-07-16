92e718cef752f2494ad26de2978eff1809bd3428
Jack Gularte <jack.gularte@ci.security>
2020-07-16 11:20:33 -0700

    refactored bmr
    I noticed that in both the male menu and female menu the coeffeicents are the same, it was just dependent on the bmr of the sex, we already called the correct bmr function earlier in main, so instead of having two menus, I store the bmr found earlier and pass it to a single menu that does the same job

--- Bmr.py
+++ Bmr.py
@@ -6,49 +6,22 @@ def female_bmr(weight, inches, age):
     return 655 + (4.3 * weight) + (4.7 * inches) - (4.7 * age)
 
 
-def male_menu():
-    print("What is your activity level?, \n"
-          "\t1. sedentary\n"
-          "\t2. lightly active\n"
-          "\t3. moderately active\n"
-          "\t4. very active\n "
-          "\t5. extremely active\n")
-    choice = input()
-    if choice == "1":
-        print("Your maintenance calories are", round(male_bmr()*1.2))
-
-    if choice == "2":
-        print("Your maintenance calories are", round(male_bmr()*1.375))
-
-    if choice == "3":
-        print("Your maintenance calories are", round(male_bmr()*1.55))
-
-    if choice == "4":
-        print("Your maintenance calories are", round(male_bmr()*1.725))
-
-    if choice == "5":
-        print("Your maintenance calories are", round(male_bmr()*1.9))
-
-
-def female_menu():
-    print("What is your activity level?,\n"
-          "\t1. sedentary\n"
-          "\t2. lightly active\n"
-          "\t3. moderately active\n"
-          "\t4. very active\n "
-          "\t5. extremely active\n")
-    choice = input()
-    if choice == "1":
-        print("Your maintenance calories are", (female_bmr()*1.2))
-
-    if choice == "2":
-        print("Your maintenance calories are", (male_bmr()*1.375))
-
-    if choice == "3":
-        print("Your maintenance calories are", (female_bmr()*1.55))
-
-    if choice == "4":
-        print("Your maintenance calories are", (female_bmr()*1.725))
-
-    if choice == "5":
-        print("Your maintenance calories are", (female_bmr()*1.9))
+def maintenance_calories(user_bmr):
+    choice = int(input("What is your activity level?\n"
+                       "\t1. sedentary\n"
+                       "\t2. lightly active\n"
+                       "\t3. moderately active\n"
+                       "\t4. very active\n "
+                       "\t5. extremely active\n"))
+    if choice == 1:
+        print("Your maintenance calories are", round(user_bmr * 1.2))
+    elif choice == 2:
+        print("Your maintenance calories are", round(user_bmr * 1.375))
+    elif choice == 3:
+        print("Your maintenance calories are", round(user_bmr * 1.55))
+    elif choice == 4:
+        print("Your maintenance calories are", round(user_bmr * 1.725))
+    elif choice == 5:
+        print("Your maintenance calories are", round(user_bmr * 1.9))
+    else:
+        print("error")
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # imports
 import Bmr
-import Macro
+from Macro import UserMacros
 
-# main
+# main, program starts here
 if __name__ == "__main__":
     height1 = int(input("Enter Height Feet: "))
     height2 = int(input("Enter Height Inches: "))
@@ -11,17 +11,19 @@ if __name__ == "__main__":
     age = int(input("Enter Age in years: "))
     inches = (height1 * 12) + height2
 
+    # calculate bmr based on sex
+    user_bmr = None
     if sex == 'm':
-        print("your basal metabolic rate is", (Bmr.male_bmr(weight, inches, age)))
-        Bmr.male_menu()
-
+        user_bmr = Bmr.male_bmr(weight, inches, age)
     elif sex == 'f':
-        print("Your basal metabolic rate is", (Bmr.female_bmr(weight, inches, age)))
-        Bmr.female_menu()
+        user_bmr = Bmr.male_bmr(weight, inches, age)
     else:
         print("enter a valid value m for male f for female")
 
-    # I assume after this first part is where you want to start using all the
+    # get maintenance calories
+    Bmr.maintenance_calories(user_bmr)
+
+    # I assume after this first part is where you want to start using all the Macros
     # functions that you created. So if this is the wrong spot you can move it around
-    userMacros = Macro.UserMacros(Bmr.male_bmr())
+    userMacros = UserMacros(user_bmr)
     userMacros.command_prompt()
